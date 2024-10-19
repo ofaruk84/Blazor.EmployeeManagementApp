3756392b6bc57a91a71d61391f8da7927275be2e
Ömer Faruk Korkmaz <korkmazfaruk84@gmail.com>
2024-10-19 13:08:14 +0300

    Profile dropdown added

---
+++ Client.BlazorUI/Pages/Authentication/ProfileDropdown.razor.cs
@@ -0,0 +1,20 @@
+﻿using Client.Lib.Utilities.Authentication;
+
+using Shared.Lib.DTOs;
+
+namespace Client.BlazorUI.Pages.Authentication
+{
+    public partial class ProfileDropdown
+    {
+        public string ProfileImage { get; set; }
+
+        async Task HandleLogout()
+        {
+            var logoutDto = new UserSessionDto();
+            var customAuthProvider = (CustomAuthenticationStateProvider)AuthStateProvider;
+            await customAuthProvider.UpdateAuthenticationState(logoutDto);
+            NavManager.NavigateTo("/login", true);
+
+        }
+    }
+}
